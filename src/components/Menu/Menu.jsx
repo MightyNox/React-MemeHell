381c765bb30d47140f8e03c6d3fd71b51da69d32
MightyNox <mateuszbruzda@interia.pl>
2019-03-04 19:38:22 +0100

    Menu buttons update

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -12,43 +12,22 @@ class Menu extends Component {
                     </div>
                 
                 
-                    <ul className="navbar-nav">
-                        <li>
-                            {/* Home */}
-                            <Link to="/">
-                                <button className="btn-menu" type="button">
-                                    <i className="fas fa-home" />
-                                </button>
-                            </Link>
-                        </li>
-                        <li>
-                            {/*  */}
-                            <Link to="/counter">
-                                <button className="btn-menu-text" type="button">
-                                    Counter
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
+                    <div className="navbar-nav">
+                        {/* Home */}
+                        <Link to="/">
+                            <button className="btn-menu" type="button">
+                                <i className="fas fa-home" />
+                            </button>
+                        </Link>
+                        {/*  */}
+                        <Link to="/counter">
+                            <button className="btn-menu-text" type="button">
+                                Counter
+                            </button>
+                        </Link>
+                    </div>
 
-                    <ul className="nav navbar-nav ml-auto">
-                        <li>
-                            {/* Register */}
-                            <Link to="/register">
-                                <button className="btn-menu" type="button">
-                                    <i className="fas fa-user-plus" />
-                                </button>
-                            </Link>
-                        </li>
-                        <li>
-                            {/* Login */}
-                            <Link to="/login">
-                                <button className="btn-menu" type="button">
-                                    <i className="fas fa-sign-in-alt" />
-                                </button>
-                            </Link>
-                        </li>
-                    </ul>
+                    {this.customizeNavBarMenu()}
 
                     <div className="dropdown">
                         <button className="btn-menu">
@@ -63,19 +42,71 @@ class Menu extends Component {
                             <Link to="/counter">
                                 Counter
                             </Link>
-                            {/* Register */}
-                            <Link to="/register">
-                                <i className="fas fa-user-plus" />
-                            </Link>
-                            {/* Login */}
-                            <Link to="/login">
-                                <i className="fas fa-sign-in-alt" />
-                            </Link>
+                            {/* Dynamic Links */}
+                            {this.customizeDropDownMenu()}
                         </div>
                     </div>
                 </nav>
             </React.Fragment>
         )
+    }
+
+    customizeNavBarMenu(){
+        if(localStorage.getItem("user")){
+            return (
+                <div className="nav navbar-nav ml-auto">
+                    {/* Logout */}
+                    <Link to="/logout">
+                        <button className="btn-menu" type="button">
+                            <i className="fas fa-sign-out-alt" />
+                        </button>
+                    </Link>
+                </div>
+            )
+        }else{
+            return(
+                <div className="nav navbar-nav ml-auto">
+                    {/* Register */}
+                    <Link to="/register">
+                        <button className="btn-menu" type="button">
+                            <i className="fas fa-user-plus" />
+                        </button>
+                    </Link>
+                    {/* Login */}
+                    <Link to="/login">
+                        <button className="btn-menu" type="button">
+                            <i className="fas fa-sign-in-alt" />
+                        </button>
+                    </Link>
+                </div>
+            )
+        }
+    }
+
+    customizeDropDownMenu(){
+        if(localStorage.getItem("user")){
+            return(
+                <div>
+                    {/* Logout */}
+                    <Link to="/logout">
+                        <i className="fas fa-sign-out-alt" />
+                    </Link>
+                </div>
+            )
+        }else{
+            return(
+                <div>
+                    {/* Register */}
+                    <Link to="/register">
+                        <i className="fas fa-user-plus" />
+                    </Link>
+                    {/* Login */}
+                    <Link to="/login">
+                        <i className="fas fa-sign-in-alt" />
+                    </Link>
+                </div>
+            )
+        }
     }
 }
 
