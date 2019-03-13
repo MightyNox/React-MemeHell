0b2d3d4f3b24a6be9b377bf5ea3a87f7633c12d4
MightyNox <mateuszbruzda@interia.pl>
2019-03-13 18:35:43 +0100

    Home component update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,42 @@ class Home extends Component {
 
                 <br/>
 
-                <h1>Home</h1>
+                <blockquote class="blockquote text-center">
+                    <h1>Featured</h1>
+
+                    <br/>
+
+                    <p class="mb-0">
+                        Meme hell is the most awesome source  <br/>
+                        of gaining knowledge and sharing experiences <br/> with another people using memes! 
+                        <span role="img"> 🤩</span>
+                    </p>
+                    <footer class="blockquote-footer">Adam Mickiewicz</footer>
+
+                    <br/>
+
+                    <p class="mb-0">
+                        It is the first time i can agree with the gentleman who wrote above. 
+                        <span role="img"> 😜</span>
+                    </p>
+                    <footer class="blockquote-footer">Juliusz Słowacki</footer>
+
+                    <br/>
+
+                    <p class="mb-0">
+                        Penguins and other specimens visit this site during the mating season. 
+                        <span role="img"> 🐧</span>
+                    </p>
+                    <footer class="blockquote-footer">Krystyna Czubówna read</footer>
+
+                    <br/>
+
+                    <p class="mb-0">
+                        Even Caesar loves Meme Hell. 
+                        <span role="img"> 🔪</span>
+                    </p>
+                    <footer class="blockquote-footer">Brutus</footer>
+                </blockquote>
             </div>
         )
     }
